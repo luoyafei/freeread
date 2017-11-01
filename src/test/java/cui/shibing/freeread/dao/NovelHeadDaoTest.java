2ae8158bd756222755e1e6e2083269b1d4dd4798
Cui shibing <643237029@qq.com>
2017-11-01 11:07:07 +0800

    添加dao层测试的基类和Pageable的一个简单实现类

--- src/test/java/cui/shibing/freeread/dao/NovelHeadDaoTest.java
+++ src/test/java/cui/shibing/freeread/dao/NovelHeadDaoTest.java
@@ -5,22 +5,18 @@ import static org.junit.Assert.assertTrue;
 import java.util.List;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import cui.shibing.freeread.CustomDaoTest;
+import cui.shibing.freeread.CustomPageable;
 import cui.shibing.freeread.model.NovelHead;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring/applicationContext.xml" })
-@Rollback(value= true)
-public class NovelHeadDaoTest {
+public class NovelHeadDaoTest extends CustomDaoTest{
+	
 	@Autowired
 	private NovelHeadMapper novelHeadMapper;
+	
 	@Test
 	public void testInsertNovelHead() {
 		NovelHead novelHead = new NovelHead();
@@ -32,101 +28,18 @@ public class NovelHeadDaoTest {
 		novelHead.setNovelChapterNum(123);
 		assertTrue(novelHeadMapper.insert(novelHead) == 1);
 	}
-
+	
 	@Test
 	public void testSelectByNameZero() {
-		List<NovelHead> novelHeads = novelHeadMapper.selectByNovleName("aaa", new Pageable() {
-
-			public int getPageNumber() {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-
-			public int getPageSize() {
-				// TODO Auto-generated method stub
-				return 5;
-			}
-
-			public int getOffset() {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-
-			public Sort getSort() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			public Pageable next() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			public Pageable previousOrFirst() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			public Pageable first() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			public boolean hasPrevious() {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-		});
+		Pageable pageable = new CustomPageable(1,5);
+		List<NovelHead> novelHeads = novelHeadMapper.selectByNovelName("aaa", pageable);
 		assertTrue(novelHeads.size()==0);
 	}
 	
 	@Test
 	public void testSelectByNameOne() {
-		List<NovelHead> novelHeads = novelHeadMapper.selectByNovleName("novel1", new Pageable() {
-
-			public int getPageNumber() {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-
-			public int getPageSize() {
-				// TODO Auto-generated method stub
-				return 5;
-			}
-
-			public int getOffset() {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-
-			public Sort getSort() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			public Pageable next() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			public Pageable previousOrFirst() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			public Pageable first() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			public boolean hasPrevious() {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-		});
-		System.out.println(novelHeads);
+		Pageable pageable = new CustomPageable(1,5);
+		List<NovelHead> novelHeads = novelHeadMapper.selectByNovelName("novel1",pageable);
 		assertTrue(novelHeads.size()==1);
 	}
 	
